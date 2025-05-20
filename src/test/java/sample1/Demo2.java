1a7c88a1382a0f5305f2bd176dc43aed6466e164
Neha_Patil0902 <nehaspatil0902@gmail.com>
2025-05-20 22:56:03 +0530

    commit and push successfully

---
+++ src/test/java/sample1/Demo2.java
@@ -0,0 +1,12 @@
+package sample1;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Shree Swami Samarth");
+		System.out.println("Hello");
+		
+	}
+
+}
